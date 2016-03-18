1952a9a90dd20365c0b020390cff5d52ecbc6f4f
root <root@localhost.(none)>
2016-03-18 18:49:45 +0800

    Tmp commit.

--- Application/Proxy/Controller/IndexController.class.php
+++ Application/Proxy/Controller/IndexController.class.php
@@ -1,8 +1,12 @@
 <?php
 namespace Proxy\Controller;
 use Think\Controller;
+use Think\Model;
+
 class IndexController extends Controller {
     public function index(){
-    	$this->display();
+    	// $this->display();
+    	$proxyModel = D("ProxyModel");
+    	dump($proxyModel->select());
     }
 }
---
+++ Application/Proxy/Model/ProxyModel.class.php
@@ -0,0 +1,13 @@
+<?php 
+namespace Proxy\Model;
+use Think\Model;
+
+/**
+* 
+*/
+class ProxyModel extends Model
+{
+	private $tableName = "Proxy";
+	private $trueTableName = "Proxy";
+}
+ ?>
