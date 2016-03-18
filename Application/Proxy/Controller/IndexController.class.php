e6e2f87c797ffc3079ff62d15cff31fb952245f6
Kevin Sum <Kevin Sum>
2016-03-19 02:27:16 +0800

    Tmp commit.

--- Application/Proxy/Controller/IndexController.class.php
+++ Application/Proxy/Controller/IndexController.class.php
@@ -8,10 +8,5 @@ class IndexController extends Controller {
     	$Model =  D("Proxy");
     	$Model->create();
     	var_dump($Model->count());
-    	// $Model->create();
-    	// $Model->name = '流年2';
-    	// $Model->add();
-    	// $a = $Model->select();
-    	// var_dump($a);
     }
 }
