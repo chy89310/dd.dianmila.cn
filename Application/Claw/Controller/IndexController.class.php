cada81978fb98b5db8f47303228385ee66b228d7
Kevin Sum <Kevin Sum>
2016-05-10 01:45:19 +0800

    Add claw list function in task view. Implement the claw index activity.

--- Application/Claw/Controller/IndexController.class.php
+++ Application/Claw/Controller/IndexController.class.php
@@ -5,9 +5,13 @@ class IndexController extends Controller {
     public function index(){
     	$condition['taskId'] = new \MongoId(I('get.taskId'));
     	$result = D('Claw')->where($condition)->field('Data')->select();
-    	var_dump($result);
-    	exit();
-    	// $this->assign('list',D('Claw')->where($condition)->select());
-    	// $this->display();    
+    	$clawArr = array();
+    	foreach ($result as $item) {
+    		if (count($item['Data']) == 1) {
+    			array_push($clawArr, $item['Data'][0]);
+    		}
+    	}
+    	$this->assign('list',$clawArr);
+    	$this->display();    
     }
 }
